3dfa9f42e42057c934f483175ec193eb542d4d48
Temitope Adeunji <temitopeadetunji@wireless-10-105-237-6.umd.edu>
2022-12-04 16:32:47 -0500

    After logout feature included

--- BulletJournal/app/src/main/java/com/example/bulletjournal/MainPage.kt
+++ BulletJournal/app/src/main/java/com/example/bulletjournal/MainPage.kt
@@ -2,6 +2,8 @@ package com.example.bulletjournal
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -21,24 +23,19 @@ class MainPage: AppCompatActivity() {
             startActivity(Intent(this@MainPage, Goals::class.java))
         }
     }
- /*
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.logout) {
-            ParseUser.logOut()
-            val intent = Intent(this@MainActivity, LaunchActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(this, "Logout Successful!!", Toast.LENGTH_SHORT).show()
-            finish()
+        if (item.itemId == R.id.logoutIcon) {
+            startActivity(Intent(this@MainPage, HomePage::class.java))
         }
-
         return super.onOptionsItemSelected(item)
     }
-*/
+
     private fun initializeUI(){
         btnJournal = findViewById(R.id.btnBulletJournal)
         btnGoals = findViewById(R.id.btnGoals)
